9ca9eba38203bef2b7f984671112ea881d720b99
David Ballester <david.ballester.mena@gmail.com>
2025-01-18 21:08:21 +0100

    Style video and canvas

--- src/asciiRenderer.js
+++ src/asciiRenderer.js
@@ -1,6 +1,7 @@
 import { getEdgesMask } from "./sobelFilter.js";
 
 const ASCII_CHARACTERS = "$#@MNBxoi;:,. ".split("");
+const getTextColor = buildGetTextColor();
 
 export function renderAscii({ canvas, imageData, columns }) {
   clearCanvas(canvas);
@@ -22,10 +23,21 @@ function renderAsciiLine({ canvas, asciiLine, lineNumber }) {
   canvas.style.letterSpacing = "3.25px";
   const context = canvas.getContext("2d");
   context.font = "8px monospace";
+  context.fillStyle = getTextColor(canvas);
   context.fillText(asciiLine.join(""), 0, lineNumber * 8);
 }
 
 function getAscii(value) {
   const index = Math.floor(value * ASCII_CHARACTERS.length);
   return ASCII_CHARACTERS[index];
+}
+
+function buildGetTextColor() {
+  let textColor;
+  return (canvas) => {
+    if (!textColor) {
+      textColor = getComputedStyle(canvas).color;
+    }
+    return textColor;
+  };
 }
