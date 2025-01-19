83f2679771d0271a3a6c49ca3ee41e44b690c94e
David Ballester <david.ballester.mena@gmail.com>
2025-01-19 12:25:55 +0100

    Reorder code to dedicate the index module to UI

--- src/asciiRenderer.js
+++ src/asciiRenderer.js
@@ -1,25 +1,45 @@
 import { getEdgesMask } from "./sobelFilter.js";
+import { asyncEvent } from "./utils.js";
+import { getIntensity } from "./imageProcessing.js";
 
-const ASCII_CHARACTERS = "$#@MNBxoi;:,. ".split("");
+const ASCII_CHARACTERS = "$#@MXxoi;:,. ".split("");
 const getTextColor = buildGetTextColor();
 
-export function renderAscii({ canvas, imageData, columns }) {
+export async function videoToAscii({
+  video,
+  canvas: asciiCanvas,
+  pixelsPerChar,
+  frameRate,
+}) {
+  await asyncEvent({ element: video, eventName: "canplay" });
+  const workingCanvas = document.createElement("canvas");
+  prepareAsciiCanvas({ canvas: asciiCanvas, video });
+  const scaleFactor = 1 / pixelsPerChar;
+  const columns = Math.floor(video.videoWidth * scaleFactor);
+  streamVideoElementToCanvas({
+    videoElement: video,
+    canvasElement: workingCanvas,
+    scaleFactor,
+    frameRate,
+    onNewFrame: (imageData) => {
+      const intensities = getIntensity({ imageData: imageData.data });
+      frameToAscii({ canvas: asciiCanvas, imageData: intensities, columns });
+    },
+  });
+}
+
+function frameToAscii({ canvas, imageData, columns }) {
   clearCanvas(canvas);
   const edgesMask = getEdgesMask({ imageData, columns });
-  const ascii = imageData.map(getAscii);
+  const ascii = imageData.map(valueToAscii);
   const asciiWithEdges = ascii.map((value, i) => edgesMask[i] || value);
   for (let i = 0; i < columns; i++) {
     const asciiLine = asciiWithEdges.slice(i * columns, (i + 1) * columns);
-    renderAsciiLine({ canvas, asciiLine, lineNumber: i + 1 });
+    lineToAscii({ canvas, asciiLine, lineNumber: i + 1 });
   }
 }
 
-function clearCanvas(canvas) {
-  const context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function renderAsciiLine({ canvas, asciiLine, lineNumber }) {
+function lineToAscii({ canvas, asciiLine, lineNumber }) {
   canvas.style.letterSpacing = "3.25px";
   const context = canvas.getContext("2d");
   context.font = "8px monospace";
@@ -27,9 +47,54 @@ function renderAsciiLine({ canvas, asciiLine, lineNumber }) {
   context.fillText(asciiLine.join(""), 0, lineNumber * 8);
 }
 
-function getAscii(value) {
+function valueToAscii(value) {
   const index = Math.floor(value * ASCII_CHARACTERS.length);
   return ASCII_CHARACTERS[index];
+}
+
+function clearCanvas(canvas) {
+  const context = canvas.getContext("2d");
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function prepareAsciiCanvas({ canvas, video }) {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.style.width = video.clientWidth;
+  canvas.style.height = video.clientHeight;
+  canvas
+    .getContext("2d")
+    .scale(window.devicePixelRatio, window.devicePixelRatio);
+}
+
+function streamVideoElementToCanvas({
+  videoElement,
+  canvasElement,
+  scaleFactor,
+  frameRate,
+  onNewFrame,
+}) {
+  canvasElement.width = videoElement.videoWidth * scaleFactor;
+  canvasElement.height = videoElement.videoHeight * scaleFactor;
+  const canvasContext = canvasElement.getContext("2d", {
+    willReadFrequently: true,
+  });
+  setInterval(() => {
+    canvasContext.drawImage(
+      videoElement,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    const imageData = canvasContext.getImageData(
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    onNewFrame(imageData);
+  }, 1e3 / frameRate);
 }
 
 function buildGetTextColor() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { renderAscii } from "./asciiRenderer.js";
-import { getIntensity } from "./imageProcessing.js";
-import { asyncEvent, waitMs } from "./utils.js";
+import { videoToAscii } from "./asciiRenderer.js";
+import { waitMs } from "./utils.js";
 import { feedWebCamToVideoElement } from "./webcamFeed.js";
 
 const enableWebcamButton = document.getElementById("enable-webcam");
@@ -11,8 +10,9 @@ enableWebcamButton.addEventListener("click", async () => {
   try {
     await feedWebCamToVideoElement(video);
     enableWebcamButton.classList.add("hidden", "live");
-    runAsciiRenderer({
+    videoToAscii({
       video,
+      canvas: document.getElementById("ascii-canvas"),
       pixelsPerChar: 8,
       frameRate: 30,
     });
@@ -24,65 +24,3 @@ enableWebcamButton.addEventListener("click", async () => {
   await waitMs(remainingTimeForSmoothAnimation);
   enableWebcamButton.removeAttribute("disabled");
 });
-
-async function runAsciiRenderer({ video, pixelsPerChar, frameRate }) {
-  await asyncEvent({ element: video, eventName: "canplay" });
-  const workingCanvas = document.createElement("canvas");
-  const asciiCanvas = document.getElementById("ascii-canvas");
-  prepareAsciiCanvas({ canvas: asciiCanvas, video });
-  const scaleFactor = 1 / pixelsPerChar;
-  const columns = Math.floor(video.videoWidth * scaleFactor);
-  streamVideoElementToCanvas({
-    videoElement: video,
-    canvasElement: workingCanvas,
-    scaleFactor,
-    frameRate,
-    onNewFrame: (imageData) =>
-      renderFrame({ imageData, canvas: asciiCanvas, columns }),
-  });
-}
-
-function prepareAsciiCanvas({ canvas, video }) {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.style.width = video.clientWidth;
-  canvas.style.height = video.clientHeight;
-  canvas
-    .getContext("2d")
-    .scale(window.devicePixelRatio, window.devicePixelRatio);
-}
-
-function renderFrame({ imageData, canvas, columns }) {
-  const intensities = getIntensity({ imageData: imageData.data });
-  renderAscii({ canvas, imageData: intensities, columns });
-}
-
-function streamVideoElementToCanvas({
-  videoElement,
-  canvasElement,
-  scaleFactor,
-  frameRate,
-  onNewFrame,
-}) {
-  canvasElement.width = videoElement.videoWidth * scaleFactor;
-  canvasElement.height = videoElement.videoHeight * scaleFactor;
-  const canvasContext = canvasElement.getContext("2d", {
-    willReadFrequently: true,
-  });
-  setInterval(() => {
-    canvasContext.drawImage(
-      videoElement,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
-    const imageData = canvasContext.getImageData(
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height
-    );
-    onNewFrame(imageData);
-  }, 1e3 / frameRate);
-}
